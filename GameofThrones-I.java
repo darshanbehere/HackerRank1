ab4b8eb2bb1d8f0dc03f5bdb5b9f13e84b4ad0ca
darshanbehere <41693727+darshanbehere@users.noreply.github.com>
2018-08-08 15:18:00 +0530

    Create GameofThrones-I.java

---
+++ GameofThrones-I.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class GameofThrones-1
+{
+  public static void main(String args[])
+  {
+    Scanner sc = new Scanner(System.in);
+    String s = sc.next();
+    int arr[] = new int[26];
+    int even=0,odd=0;
+    for(int i=0;i<s.length();i++)
+    {
+      arr[s.charAt(i)-'a']++;
+    }
+    for(int i=0;i<arr.length();i++)
+    {
+      if(arr[i]%2!=0)
+        odd++;
+    }
+    if(odd>1)
+      System.out.println("NO");
+    else
+      System.out.println("YES");
+  }
+}
