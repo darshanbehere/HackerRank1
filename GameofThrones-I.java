bbab6c20ec7246c4baf26c5d3300b076a8cda8e2
darshanbehere <41693727+darshanbehere@users.noreply.github.com>
2018-08-08 15:22:13 +0530

    Update GameofThrones-I.java

--- GameofThrones-I.java
+++ GameofThrones-I.java
@@ -11,7 +11,7 @@ class GameofThrones-I
     {
       arr[s.charAt(i)-'a']++;
     }
-    for(int i=0;i<arr.length();i++)
+    for(int i=0;i<arr.length;i++)
     {
       if(arr[i]%2!=0)
         odd++;
