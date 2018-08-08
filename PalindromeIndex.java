a68c7a40a995652e97eaf3c95fa03d4e264872ae
darshanbehere <41693727+darshanbehere@users.noreply.github.com>
2018-08-08 07:43:38 +0530

    1st PalindromeIndex.java program

---
+++ PalindromeIndex.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class PalindromeIndex{
+	public static void main(String args[]){
+	Scanner sc = new Scanner(System.in);
+	String s = sc.next();
+	int end = s.length()-1;
+	int c=-1;
+	for(int i=0;i<s.length()/2;i++){
+			char st = s.charAt(i);
+			char ed = s.charAt(end)
+			if(st!=end){
+				boolean flag=true;
+				for(int j=i,en=end-1,c=end;j<s.length()/2;j++,en--){
+					st=s.charAt(j);
+					ed=s.charAt(en);
+					if(st!=ed){
+						flag=false;
+						c=-1;
+						break;
+					}
+				}
+				if(flag)
+					break;
+			for(int j=i+1,en=end,c=i;j<s.length()/2;j++,en--){
+					st=s.charAt(j);
+					ed=s.charAt(en);
+					if(st!=ed){
+						flag=false;
+						c=-1;
+						break;
+					}
+				}
+			}
+			end--;
+		}
+		System.out.println(c);
+	}
+}
