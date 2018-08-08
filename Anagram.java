c900c514532f553e6889c78435e7cb95b6a07c32
darshanbehere <41693727+darshanbehere@users.noreply.github.com>
2018-08-08 14:32:04 +0530

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -2,12 +2,13 @@ import java.util.*;
 class Anagram{
   public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
-    String s = s.next();
+    String s = sc.next();
     int len = s.length();
     int count=0;
     int arr1[]=new int[26];
     int arr2[]=new int[26];
-    if(len%2==0){
+    if(len%2==0)
+    {
       for(int i=0;i<len/2;i++)
       {
         char c1 = s.charAt(i);
@@ -15,7 +16,7 @@ class Anagram{
         arr1[c1-'a']++;
         arr2[c2-'a']++;
       }
-      for(int i=0;i<arr.length;i++)
+      for(int i=0;i<arr1.length;i++)
       {
         if(arr1[i]>arr2[i])
         {
@@ -23,6 +24,8 @@ class Anagram{
         }
       }
     }
+    else
+      count=-1;
     System.out.println(count);
   }
 }
