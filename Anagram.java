f2e61dfd1cc55c8fbd2942c16aa341dd6d58e192
darshanbehere <41693727+darshanbehere@users.noreply.github.com>
2018-08-08 14:25:08 +0530

    anagram

---
+++ Anagram.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Anagram{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    String s = s.next();
+    int len = s.length();
+    int count=0;
+    int arr1[]=new int[26];
+    int arr2[]=new int[26];
+    if(len%2==0){
+      for(int i=0;i<len/2;i++)
+      {
+        char c1 = s.charAt(i);
+        char c2 = s.charAt((len/2)+i);
+        arr1[c1-'a']++;
+        arr2[c2-'a']++;
+      }
+      for(int i=0;i<arr.length;i++)
+      {
+        if(arr1[i]>arr2[i])
+        {
+          count=count+(arr1[i]-arr2[i]);
+        }
+      }
+    }
+    System.out.println(count);
+  }
+}
