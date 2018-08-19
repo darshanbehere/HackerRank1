65fbc5efa1bd9e542c036755ad3ebf6bac8fa4ac
darshanbehere <41693727+darshanbehere@users.noreply.github.com>
2018-08-19 12:41:27 +0530

    Create LargestPrimeFactor.java

---
+++ LargestPrimeFactor.java
@@ -0,0 +1,10 @@
+class LargestPrimeFactor
+{
+  public static void main(String args[])
+  {
+    Scanner sc = new Scanner(System.in);
+    long no = sc.nextLong();
+    long sqno = Math.sqrt(no);
+    System.out.println(no);
+  }
+}
